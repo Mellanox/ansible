ff0dd1b80c0a3cfda2ccc6b63eb7676ac06ef657
Samer Deeb <samerd@mellanox.com>
2017-12-05 23:00:27 +0000

    All mlnxos modules for testing
    Signed-off-by: Samer Deeb <samerd@mellanox.com>

--- lib/ansible/module_utils/network/mlnxos/mlnxos.py
+++ lib/ansible/module_utils/network/mlnxos/mlnxos.py
@@ -17,6 +17,7 @@
 # You should have received a copy of the GNU General Public License
 # along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
 #
+import json
 
 from ansible.module_utils._text import to_text
 from ansible.module_utils.basic import env_fallback
@@ -26,34 +27,11 @@ from ansible.module_utils.network.common.utils import to_list, EntityCollection
 _DEVICE_CONFIGS = {}
 _CONNECTION = None
 
-mlnxos_provider_spec = {
-    'host': dict(),
-    'port': dict(type='int'),
-    'username': dict(fallback=(env_fallback,
-                               ['ANSIBLE_NET_USERNAME'])),
-    'password': dict(fallback=(env_fallback,
-                               ['ANSIBLE_NET_PASSWORD']), no_log=True),
-    'ssh_keyfile': dict(fallback=(env_fallback,
-                                  ['ANSIBLE_NET_SSH_KEYFILE']), type='path'),
-    'authorize': dict(fallback=(env_fallback,
-                                ['ANSIBLE_NET_AUTHORIZE']), type='bool'),
-    'auth_pass': dict(fallback=(env_fallback,
-                                ['ANSIBLE_NET_AUTH_PASS']), no_log=True),
-    'timeout': dict(type='int')
-}
-mlnxos_argument_spec = {
-    'provider': dict(type='dict', options=mlnxos_provider_spec),
-}
-
 command_spec = {
     'command': dict(key=True),
     'prompt': dict(),
     'answer': dict()
 }
-
-
-def get_provider_argspec():
-    return mlnxos_provider_spec
 
 
 def get_connection(module):
@@ -100,3 +78,129 @@ def load_config(module, config):
         conn.edit_config(config)
     except ConnectionError as exc:
         module.fail_json(msg=to_text(exc))
+
+
+def show_cmd(module, cmd, json_fmt=True, fail_on_error=True):
+    if json_fmt:
+        cmd += " | json-print"
+    conn = get_connection(module)
+    command_obj = to_commands(module, to_list(cmd))[0]
+    try:
+        out = conn.get(**command_obj)
+    except ConnectionError:
+        if fail_on_error:
+            raise
+        return None
+
+    if json_fmt:
+        out_list = out.split('\n', 1)
+        line = out_list[0].strip()
+        if line and line[0] not in ("[", "{"):
+            out = out_list[1]
+        try:
+            cfg = json.loads(out)
+        except ValueError:
+            module.fail_json(
+                msg="got invalid json",
+                stderr=to_text(out, errors='surrogate_then_replace'))
+    else:
+        cfg = to_text(out, errors='surrogate_then_replace').strip()
+    return cfg
+
+
+def get_interfaces_config(module, interface_type, json_fmt=True,
+                          summary=False):
+    cmd = "show interfaces %s" % interface_type
+    if summary:
+        cmd += " summary"
+    return show_cmd(module, cmd, json_fmt)
+
+
+def get_bgp_summary(module):
+    cmd = "show ip bgp summary"
+    return show_cmd(module, cmd, json_fmt=False, fail_on_error=False)
+
+
+class BaseMlnxosModule(object):
+
+    def __init__(self):
+        self._module = None
+        self._commands = list()
+        self._current_config = None
+        self._required_config = None
+
+    def init_module(self):
+        pass
+
+    def load_current_config(self):
+        pass
+
+    def get_required_config(self):
+        pass
+
+    # pylint: disable=unused-argument
+    def check_declarative_intent_params(self, result):
+        return None
+
+    def validate_param_values(self, obj, param=None):
+        if param is None:
+            param = self._module.params
+        for key in obj:
+            # validate the param value (if validator func exists)
+            try:
+                validator = getattr(self, 'validate_%s' % key)
+                if callable(validator):
+                    validator(param.get(key))
+            except AttributeError:
+                pass
+
+    @classmethod
+    def get_config_attr(cls, item, arg):
+        return item.get(arg)
+
+    @classmethod
+    def get_mtu(cls, item):
+        mtu = cls.get_config_attr(item, "MTU")
+        mtu_parts = mtu.split()
+        try:
+            return int(mtu_parts[0])
+        except ValueError:
+            return None
+
+    def validate_mtu(self, value):
+        if value and not 1500 <= int(value) <= 9612:
+            self._module.fail_json(msg='mtu must be between 1500 and 9612')
+
+    def generate_commands(self):
+        pass
+
+    def run(self):
+        self.init_module()
+
+        result = {'changed': False}
+
+        self.get_required_config()
+        self.load_current_config()
+
+        self.generate_commands()
+        result['commands'] = self._commands
+
+        if self._commands:
+            if not self._module.check_mode:
+                load_config(self._module, self._commands)
+            result['changed'] = True
+
+        failed_conditions = self.check_declarative_intent_params(result)
+
+        if failed_conditions:
+            msg = 'One or more conditional statements have not been satisfied'
+            self._module.fail_json(msg=msg,
+                                   failed_conditions=failed_conditions)
+
+        self._module.exit_json(**result)
+
+    @classmethod
+    def main(cls):
+        app = cls()
+        app.run()
+
